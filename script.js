5a10007f982f020595db283b59fd0d61bf4a9e2d
ChaouiZ <106296433+ChaouiZ@users.noreply.github.com>
2024-08-03 20:14:02 -0500

    Added table with basic styling

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 const myLibrary = [];
 
-function Book(title,author,numOfPages,read) {
-    this.title = title;
-    this.author = author;
-    this.numOfPages = numOfPages;
-    this.read = read;
+function Book(title, author, numOfPages, read) {
+  this.title = title;
+  this.author = author;
+  this.numOfPages = numOfPages;
+  this.read = read;
 
-    this.info = function() {
-        let readStatus = '';
-        if (this.read === true) {
-            readStatus = 'Read';
-        } else {
-            readStatus = 'Unread'
-        }
-
-        console.log(`${title}, ${author}, ${numOfPages} pages, ${readStatus}`);
-
+  this.info = function () {
+    let readStatus = "";
+    if (this.read === true) {
+      readStatus = "Read";
+    } else {
+      readStatus = "Unread";
     }
+
+    console.log(`${title}, ${author}, ${numOfPages} pages, ${readStatus}`);
+  };
 }
+
+let lOtr = new Book("Lord of the Rings", "J.R.R Tolkien", 290, true);

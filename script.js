e3328c2b94c18d1bb5b75dae8769ff2df2ef7d1d
ChaouiZ <106296433+ChaouiZ@users.noreply.github.com>
2024-08-05 20:14:46 -0500

    Added input. Input entered into Book constructor and  pushed to Book array

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
+
+const submit = document.querySelector('.submit');
+
+
 const myLibrary = [];
 
 function Book(title, author, numOfPages, read) {
-  this.title = title;
-  this.author = author;
-  this.numOfPages = numOfPages;
-  this.read = read;
-
-  this.info = function () {
-    let readStatus = "";
-    if (this.read === true) {
-      readStatus = "Read";
-    } else {
-      readStatus = "Unread";
-    }
-
-    console.log(`${title}, ${author}, ${numOfPages} pages, ${readStatus}`);
-  };
+    this.title = title;
+    this.author = author;
+    this.numOfPages = numOfPages;
+    this.read = read;
+    
+    this.info = function () {
+        let readStatus = "";
+        if (this.read === true) {
+            readStatus = "Read";
+        } else {
+            readStatus = "Unread";
+        }
+        
+        console.log(`${title}, ${author}, ${numOfPages} pages, ${readStatus}`);
+    };
 }
 
+
 let lOtr = new Book("Lord of the Rings", "J.R.R Tolkien", 290, true);
+
+
+submit.addEventListener('click', () => {
+    const titleInput = document.querySelector("#title-input").value;
+    const authorInput = document.querySelector('#author-input').value;
+    const pagesInput = document.querySelector('#pages-input').value;
+    const book = new Book(titleInput, authorInput, pagesInput);
+    myLibrary.push(book);
+    console.log(myLibrary);
+})
